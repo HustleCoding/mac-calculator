d95261d75e2894fa2a148446a3a33b36a0023980
HustleCoding <f.dobinciuc7@gmail.com>
2021-10-22 16:00:43 +0300

    [TASK 3] - main file configured

--- src/App.js
+++ src/App.js
@@ -1,1 +1,10 @@
+import "./style.css";
+import { render } from "react-dom";
 
+const App = () => {
+  return <div></div>;
+};
+
+export default App;
+
+render(<App />, document.getElementById("root"));
