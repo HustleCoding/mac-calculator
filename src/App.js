14de7fa6ac6b4c2f355251171fcf127caa8627d0
HustleCoding <f.dobinciuc7@gmail.com>
2021-10-27 18:15:22 +0300

    [TASK 7] - refactoring on App.js and Button.js files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import "./style.css";
 import { render } from "react-dom";
 import Button from "./Button";
+import { useState, useEffect } from "react";
 
 const App = () => {
+  const [preState, setPreState] = useState("");
+  const [curState, setCurState] = useState("");
+  const [input, setInput] = useState("0");
+  const [operator, setOperator] = useState(null);
+  const [total, setTotal] = useState(false);
+
+  useEffect(() => {
+    setInput(curState);
+  }, [curState]);
+
+  useEffect(() => {
+    setInput("0");
+  }, []);
+
   return (
     <div>
-      <Button />
+      <Button
+        preState={preState}
+        setPreState={setPreState}
+        setCurState={setCurState}
+        input={input}
+        operator={operator}
+        setOperator={setOperator}
+        total={total}
+        setTotal={setTotal}
+        setInput={setInput}
+        curState={curState}
+      />
     </div>
   );
 };
--- src/Button.js
+++ src/Button.js
@@ -1,23 +1,20 @@
-import { useState, useEffect } from "react";
+import App from "./App";
 import MacToolbar from "./MacToolbar";
 import Screen from "./Screen";
 import NumberFormat from "react-number-format";
 
-const Button = () => {
-  const [preState, setPreState] = useState("");
-  const [curState, setCurState] = useState("");
-  const [input, setInput] = useState("0");
-  const [operator, setOperator] = useState(null);
-  const [total, setTotal] = useState(false);
-
-  useEffect(() => {
-    setInput(curState);
-  }, [curState]);
-
-  useEffect(() => {
-    setInput("0");
-  }, []);
-
+const Button = ({
+  setCurState,
+  setPreState,
+  setTotal,
+  setOperator,
+  setInput,
+  curState,
+  total,
+  preState,
+  operator,
+  input,
+}) => {
   const inputNum = (e) => {
     if (curState.includes(",") && e.target.innerText === ",") return;
 
